e6c240baeef900e4062b537ea31e5bb1814804c5
Julie Erlemeier <jmerlemeier@gmail.com>
2019-11-02 17:11:39 -0700

    All tests pass,

--- __tests__/validator.test.js
+++ __tests__/validator.test.js
@@ -36,27 +36,11 @@ describe('validator module performs basic validation of strings, numbers, arrays
     let func = () => {};
     expect(validator.isFunction(func)).toBeTruthy();
   });
-});
 
-describe('validator module performs complex validations', () => {
-
-  // it('validates the presence of required object properties at any level', () => {
-  //   // i.e. does person.hair.color exist and have a good value, not just person.hair
-  //   let person = {name: 'James', legs: 2, hair: {color: 'brown', length: 'short'}};
-  //   //is animal an object
-  //   expect(validator.isObject(obj)).toBeTruthy();
-  //   //does person.hair.color exist?
-  //   expect(validator.requiredProperty(object)).toBeTruthy();
-  // });
-
-  // it('validates the proper types of object properties', () => {
-  //   // i.e. person.name must be a string, etc.
-  //   expect(true).toBeFalsy();
-  // });
 
   it('validates the types of values contained in an array', () => {
     let arr = ['a', 'b', 'c', 'd'];
-    let badArr = ['a', 3, 'c', 18]
+    let badArr = ['a', 3, 'c', 18];
     //check that array is array
     expect(validator.isArray(arr)).toBeTruthy(); //if true, passes.
     expect(validator.isArray(badArr)).toBeTruthy(); //if true, passes.
@@ -64,13 +48,5 @@ describe('validator module performs complex validations', () => {
     expect(validator.arrayTypesSame(arr)).toBeTruthy();
     expect(validator.arrayTypesSame(badArr)).toBeFalsy();
   });
-
-
-  // it('validates a value array against an approved list', () => {
-  //   // i.e. a string might only be allowed to be "yes" or "no"
-  //   expect(true).toBeFalsy();
-  // });
-
-  // TODO: Cover so, so many more cases
 
 });
